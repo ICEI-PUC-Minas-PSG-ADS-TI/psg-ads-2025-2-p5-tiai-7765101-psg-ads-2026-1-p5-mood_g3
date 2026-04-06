ee099f5d5a8abf4451da6ca3df88ab1ebc110672
cammilamachado <camsmpm@gmail.com>
2026-04-06 18:39:09 -0300

    login e cadastro de usuario

--- src/projeto/urls.py
+++ src/projeto/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
+from core.views import register_view
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path("register/", register_view, name="register"),
     path("", include("core.urls")),
 ]
